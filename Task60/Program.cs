e6cb88b6d3e7a04033d322e3ca257f958e223af1
OVK1976 <olegkalinin@yandex.ru>
2022-10-31 15:57:51 +0400

    Final Ver

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -7,3 +7,46 @@
 27(0, 0, 1) 90(0, 1, 1)
 26(1, 0, 1) 55(1, 1, 1) 
 */
+
+Console.WriteLine("Вывод элементов трехмерного массива");
+Console.WriteLine("Создан массив:");
+int[,,] NewArray = GetArray();
+PrintArray(NewArray);
+
+int[,,] GetArray()
+{
+    int[,,] array = new int[2, 2, 2];
+    Random rnd = new();
+    for (int k = 0; k < 2; k++)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+
+                {
+                    array[i, j, k] = rnd.Next(1, 10);
+                }
+            }
+
+        }   
+
+    }
+    return array;
+}
+void PrintArray(int[,,] array)
+{
+
+    for (int k = 0; k < array.GetLength(2); k++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {        
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"  {array[i, j, k]} ({i}, {j}, {k})  ");
+                }
+                Console.WriteLine();
+        }
+         
+    }
+}
