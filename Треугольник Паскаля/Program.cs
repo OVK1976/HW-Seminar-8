eed95cac80bfd86abc2ae20589121a17cc90cdcb
OVK1976 <olegkalinin@yandex.ru>
2022-11-01 10:03:18 +0400

    Correct Print
    Отформатирован вывод треугольника Паскаля

---
+++ Треугольник Паскаля/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задание дополнительно
+ * Дополнительно 
+ * Напишите программу выводящую треугольник Паскаля*/
+
+Console.WriteLine("Введите количество строк треугольника Паскаля:");
+int n=int.Parse(Console.ReadLine());
+for ( int k = 0; k < n; k++)
+{
+    for (int m = 0; m <= (n - k); m++)
+    {
+       
+            Console.Write("   ");
+        
+    }
+    for (int m = 0; m <= k; m++)
+    {
+        
+            Console.Write("    ");
+        
+        Console.Write(factorial(k) / (factorial(m) * factorial(k - m)));
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+Console.ReadLine();
+
+
+#region Нахождение факториала
+
+int factorial(int n)
+{
+    int F = 1;
+    for ( int i = 1; i <= n; i++)
+    {
+        F *= i;
+    }
+    return F;
+}
+#endregion
